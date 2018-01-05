57bc026cf60b264e6f4465588a0582e559803776
adams003 <adamspouele@gmail.com>
2018-01-05 15:40:59 +0100

    Change Date

--- app/src/main/java/org/esiea/pouele_nemmene/intechtpapp/MainActivity.java
+++ app/src/main/java/org/esiea/pouele_nemmene/intechtpapp/MainActivity.java
@@ -1,10 +1,12 @@
 package org.esiea.pouele_nemmene.intechtpapp;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.text.DateFormat;
@@ -12,21 +14,35 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+     DatePickerDialog dpd = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv_hw = (TextView)findViewById(R.id.tv_hello_world);
+        final TextView tv_hw = (TextView)findViewById(R.id.tv_hello_world);
 
         String now = DateUtils.formatDateTime(getApplicationContext(), (new Date()).getTime(), DateFormat.FULL);
         tv_hw.setText(now);
 
         Button btn_hw = (Button)findViewById(R.id.button);
+
+        DatePickerDialog.OnDateSetListener odsl = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                tv_hw.setText(i2+"/"+i1+"/"+i);
+            }
+        };
+
+        dpd = new DatePickerDialog(this, odsl, 2018, 2, 10);
+
         btn_hw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),getString(R.string.app_name), Toast.LENGTH_SHORT).show();
+                dpd.show();
+                Toast.makeText(getApplicationContext(), "Selection de date", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
