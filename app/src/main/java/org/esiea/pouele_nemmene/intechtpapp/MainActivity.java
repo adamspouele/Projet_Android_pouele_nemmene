e4aef2f999bc7245d2f664550fa51c671c7f6950
adams003 <adamspouele@gmail.com>
2018-01-05 14:55:26 +0100

    change hello world to current date

--- app/src/main/java/org/esiea/pouele_nemmene/intechtpapp/MainActivity.java
+++ app/src/main/java/org/esiea/pouele_nemmene/intechtpapp/MainActivity.java
@@ -2,6 +2,11 @@ package org.esiea.pouele_nemmene.intechtpapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv_hw = (TextView)findViewById(R.id.tv_hello_world);
+
+        String now = DateUtils.formatDateTime(getApplicationContext(), (new Date()).getTime(), DateFormat.FULL);
+
+        String hw_string = getString(R.string.hello_world);
+        tv_hw.setText(now);
     }
 }
